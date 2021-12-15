b286f3ce5bf1af0a1ae97c7f4ab69c14c1c8d627
jhkim <alicelikesbab@gmail.com>
2021-12-15 17:44:46 +0900

    생활코딩으로리엑트공부시작

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,33 +1,73 @@
-  import React, { useState } from 'react';
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let [글제목,글제목변경] = useState('남자 코트 추천');
-  let [글제목2,글제목변경2] = useState(['여자 코트 추천','판교 우동 맛집']);
-
+  let [글제목,글제목변경] = useState(['남자 코트 추천','여자 코트 추천','아동 코트 추천','시니어 코트 추천']);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '판교 고기 맛집';
+  //따봉변경(10); //여기에 직접쓰면 재랜더링 무한으로 일어나기 때문에 쓰면 안 됨
+  function 제목바꾸기(){
+    var newArray = [...글제목]; //글제목에 있던 0번째 데이터를 여자코트 추천으로 바꿈
+    newArray[0] = '남아 외투 추천';
+    newArray[1] = '여아 외투 추천';
+    newArray[2] = '성인 외투 추천';
+    newArray[3] = '성인 신발 추천';
+    글제목변경(newArray);
+  }
   
   return (
     <div className="App">
       <div className='black-nav'>
-         <div>개발 Blog</div>     
+      <div>개발 Blog</div>     
       </div>
 
-      <div className='list'>
-        <h3>{ 글제목 }</h3>
+      <button onClick={ 제목바꾸기 }> 가나다순 정렬 </button>
+
+      <div className='list'>        
+        <h3>{ 글제목[0]}<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h3> 
         <p>2월 17일 발행</p>
         <hr/>
       </div> 
 
       <div className='list'>
-        <h3>{ 글제목[0] }</h3>
-        <p>1월 7일 발행</p>
+        <h3>{ 글제목[1]}<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h3> 
+        <p>2월 17일 발행</p>
         <hr/>
-      </div>   
+      </div> 
+  
+      <div className='list'>
+        <h3>{ 글제목[2]}<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h3> 
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div> 
+  
+      <div className='list'>
+        <h3>{ 글제목[3]}<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h3> 
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div> 
+
+      <Modal /> {/* <Modal></Modal> */}
+  
     </div>
   );
+}
+
+//Component 만들기
+function Modal(){
+  return(
+    <>
+    <div className='modal'>
+      <h2>{ 글제목[0] }</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+    </>
+  )
 }
 
 export default App;
---
+++ React/react_app/src/App.js
@@ -0,0 +1,15 @@
+// class type
+import React, { Component } from "react";
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        Hello, react!!
+      </div>
+    );
+  }
+}
+
+export default App;
