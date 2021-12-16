136b1ac5009c13ad89df550b8d38a2c844aabf7c
jhkim <alicelikesbab@gmail.com>
2021-12-16 17:17:39 +0900

    1216OpenTutorial-14

--- React/react_app/src/App.js
+++ React/react_app/src/App.js
@@ -1,12 +1,23 @@
 // class type
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
+import Content from "./components/Content";
 
 class App extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className="App">
-        Hello, react!!
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <hr />
+        <TOC></TOC>
+        <hr />
+        <Content
+          title="HTML"
+          desc="HTML is Hyper Text Markup Language"
+        ></Content>
       </div>
     );
   }
---
+++ React/react_app/src/components/TOC.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <li>
+          <a href="1.html">HTML</a>
+        </li>
+        <li>
+          <a href="2.html">CSS</a>
+        </li>
+        <li>
+          <a href="3.html">JavaScript</a>
+        </li>
+      </nav>
+    );
+  }
+}
+export default TOC;
