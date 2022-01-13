b354a3c685eb59ccd4ee7c2a053a3a2a5678cbb5
jhkim <alicelikesbab@gmail.com>
2022-01-13 17:12:57 +0900

    refactoring , Event 1/2

--- React/react_app_ot/src/App.js
+++ React/react_app_ot/src/App.js
@@ -9,25 +9,54 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "welcome",
       subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "Welcome", desc: "Hello React !!" },
       contents: [
         { id: 1, title: "HTML", desc: "HTML is for information" },
         { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JAVASCRIPT", desc: "JAVASCRIPT is for interactive" },
       ],
     };
   }
   render() {
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject
+        {/* 2. Event 적용 후 Subject컴포넌트 사용 */}
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-        ></Subject>
+        ></Subject> */}
+        {/* 1. Event 적용 */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                console.log(e);
+                e.preventDefault();
+                this.setState({
+                  mode: "read",
+                });
+              }.bind(this)}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
+
         <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is Hyper Text Markup Language"
-        ></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
