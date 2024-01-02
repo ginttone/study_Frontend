87bd056483337e3769a4983933cfe3fe334f811e
unknown <alicelikesbab@gmail.com>
2024-01-02 11:37:41 +0900

    [CLEAR]

--- React/rnd_ot/src/App.js
+++ React/rnd_ot/src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
-import {Input} from "reactstrap";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <React.Fragment>
-      <h1> say hi </h1>
-      <Input />
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
