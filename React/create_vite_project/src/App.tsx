bc7ab2a0a2341bfe0fa14c9d96b94662f260a999
unknown <alicelikesbab@gmail.com>
2024-03-05 11:07:15 +0900

    test

--- React/create_vite_project/src/App.tsx
+++ React/create_vite_project/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Template from './component/Todo_gerineLenara/Template';
 
 const App: React.FC = () => {
 
   return (
-    <div className='App'>
-      <Template>
-      </Template>
-    </div>
-
+    <React.Fragment>
+      <h1>test</h1>
+    </React.Fragment>
   )
 }
 
--- React/create_vite_project/src/component/Todo_gerineLenara/TodoInsert.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TodoInsert() {
-    return (
-        <div>TodoInsert</div>
-    )
-}
